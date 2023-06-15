2473e0c2038c4fa918126b806e3a1a4c2bb5a551
root <root@inwi.168.1.22>
2023-06-15 19:50:13 -0400

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-int	main(void)
+	/**
+	* main - this function printf a string.
+	* puts : this is a function that prints the string.
+	*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
