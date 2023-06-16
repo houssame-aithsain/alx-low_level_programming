72c45dd475fbc9c63cb4bb4d491abfc0db62a615
root <root@inwi.168.1.22>
2023-06-15 20:14:01 -0400

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
+
+/**
+ * main - main function "that will print the size of vars"
+ * Return: it will return 0 "if the programme exits"
+ */
 int main(void)
 {
-    printf("Size of a char: %ld byte(s)\n", sizeof(char));
-    printf("Size of an int: %ld byte(s)\n", sizeof(int));
-    printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %ld byte(s)\n", sizeof(float));
-
-    return 0;
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
 }
 
