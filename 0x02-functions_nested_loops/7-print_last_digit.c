4052753cc07255862e9f2d00266ea241f8c5d2f4
root <root@inwi.168.1.22>
2023-06-20 08:41:42 -0400

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+	* print_last_digit - is the main "function"
+	* @num: var
+	* Return: it  return "0" or "1" if c is lower
+*/
+int print_last_digit(int num)
+{
+	if (num < 0)
+	{
+		num = num * -1;
+		_putchar((num % 10) + 48);
+		return (0);
+	}
+	_putchar((num % 10) + 48);
+	return (0);
+}
