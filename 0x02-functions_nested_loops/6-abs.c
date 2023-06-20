4de359ab5eb518b569f1e2db6245932db4cffcd4
root <root@inwi.168.1.22>
2023-06-20 08:21:58 -0400

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
+/**
+	* _abs - is the main "function"
+	* @j: var
+	* Return: it  return "0" or "1" if c is lower
+*/
 int _abs(int j)
 {
-    if (j < 0)
-        return (j * -1);
-    return (j);
+	if (j < 0)
+		return (j * -1);
+	return (j);
 }
