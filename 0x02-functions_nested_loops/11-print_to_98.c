4f10a5938550e00e0258973d39c8781275e134c8
root <root@inwi.168.1.22>
2023-06-20 10:38:25 -0400

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_number - Prints a number using _putchar
+ * @n: The number to be printed
+ */
+void print_number(int n)
+{
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar('0' + (n % 10));
+}
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: The starting number
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			print_number(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			print_number(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+
+	_putchar('\n');
+}
