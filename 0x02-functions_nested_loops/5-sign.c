dad917f309379f4b32af35d23d7152c92497ae77
root <root@inwi.168.1.22>
2023-06-20 08:10:06 -0400

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+	* print_sign - is the main "function"
+	* @n: var
+	* Return: it  return "0" or "1" if c is lower
+*/
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
