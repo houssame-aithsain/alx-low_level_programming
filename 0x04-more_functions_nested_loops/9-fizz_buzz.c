8236acf1979aea2083d4b806954326584fbc7d87
root <root@inwi.168.1.22>
2023-06-23 12:33:25 -0400

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-# include <stdio.h>
+# include "main.c"
 
 /**
  * main - Entry point for the FizzBuzz program
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 # define MAIN_H
 
 # include <unistd.h>
+# include <stdio.h>
 
 int _isupper(int c);
 int _isdigit(int c);
