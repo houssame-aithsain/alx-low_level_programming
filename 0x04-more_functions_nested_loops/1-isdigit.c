f74daa3d5311d0bd5c0649516a80978902e81849
root <root@inwi.168.1.22>
2023-06-23 11:05:29 -0400

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,11 @@
 /**
  * _isdigit - checks if a character is a digit (0-9)
  * @c: The character to check
- *
  * Return: Returns 1 if the character is a digit, 0 otherwise.
  */
-
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
