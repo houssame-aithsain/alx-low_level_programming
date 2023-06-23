c9fcdbae69e7320edb0bedeb76f0d5e94ee6c15c
root <root@inwi.168.1.22>
2023-06-23 12:46:59 -0400

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,20 +6,19 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, spaces;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
 		return;
-	}
-
 	for (i = 0; i < size; i++)
 	{
+		spaces = size - i - 1;
+
+		for (j = 0; j < spaces; j++)
+			_putchar(' ');
 		for (j = 0; j <= i; j++)
-		{
 			_putchar('#');
-		}
 		_putchar('\n');
 	}
 }
