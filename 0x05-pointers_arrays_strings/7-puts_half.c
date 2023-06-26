3064c37f378a945d5df7788c1fb81434ceda53bd
root <root@inwi.168.1.22>
2023-06-26 10:31:15 -0400

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string.
+ * @str: Pointer to the string.
+ *
+ * Description: This function takes a string as input and prints the second
+ *              half of the string. It calculates the length of the string
+ *              by counting the number of characters until the null terminator.
+ *              Then it determines the starting index `i` for printing the
+ *              second half based on whether the length is odd or even. If the
+ *              length is odd, `i` is set to len / 2, otherwise, it is set to
+ *              (len - 1) / 2. Finally, it uses a loop to print the characters
+ *              from index `i` until the end of the string.
+ */
+void puts_half(char *str)
+{
+	int len;
+	int i;
+
+	len = 0;
+	i = 0;
+	while (str && str[len])
+		len++;
+
+	if (((len - 1) % 2))
+		i = (len / 2);
+	else
+		i = ((len - 1) / 2);
+
+	while (str && i < len)
+		_putchar(str[i++]);
+}
