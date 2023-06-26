0c8f19bb9244ea034db785ddb92da7572cef996e
root <root@inwi.168.1.22>
2023-06-26 10:11:57 -0400

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string in place.
+ * @s: Pointer to the string.
+ *
+ * Description: This function takes a string as input and reverses its
+ *              order in place. It uses two variables, `i` and `len`, to
+ *              iterate through the string from both ends towards the
+ *              middle. The characters at corresponding positions are
+ *              swapped until the midpoint is reached. The function does
+ *              not return a value.
+ */
+void rev_string(char *s)
+{
+	int i;
+	int ll;
+	int len;
+	char tmp;
+
+	i = 0;
+	len = 0;
+	while (s && s[len])
+		len++;
+	ll = len;
+	while (i < (ll / 2))
+	{
+		len--;
+		tmp = s[i];
+		s[i] = s[len];
+		s[len] = tmp;
+		i++;
+	}
+}
