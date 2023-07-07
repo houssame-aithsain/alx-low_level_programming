fb20b6371e304e8b9a85a4c95f2a9c96ba0bca24
root <root@inwi.168.1.22>
2023-07-07 17:18:33 -0400

    lib

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Compile each .c file into .o object files
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
 
-# Create the static library from the object files
 ar rcs liball.a *.o
 
-# Clean up the object files
-rm *.o
+ranlib liball.a
