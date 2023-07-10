f4168814f8735370ba0375cf1d9eb4f46bba3c17
root <root@inwi.168.1.22>
2023-07-10 12:44:25 -0400

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,14 +13,11 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i = 0;
 
-	arr = malloc(size + 1);
+	if (!size)
+		return (NULL);
+	arr = malloc(size);
 	if (!arr)
 		return (NULL);
-	if (!size)
-	{
-		arr[0] = c;
-		return (arr);
-	}
 	for (; i < size; i++)
 		arr[i] = c;
 	arr[i] = 0;
