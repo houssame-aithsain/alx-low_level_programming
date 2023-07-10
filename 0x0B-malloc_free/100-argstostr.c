fbf65f93898a6cf292e7d5fbc089a08454ef2840
root <root@inwi.168.1.22>
2023-07-10 15:32:10 -0400

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,92 +1,49 @@
 #include "main.h"
 
 /**
- * ft_p_strlen - Calculates the total length of all strings in an array.
- * @av: The array of strings.
+ * argstostr - Concatenates all arguments into a single string.
+ * @ac: The number of arguments.
+ * @av: The array of arguments.
  *
- * Return: The total length of all strings.
- */
-int ft_p_strlen(char **av)
-{
-	int len = 0;
-	int i = 1;
-	int n;
-
-	while (av[i])
-	{
-		n = 0;
-		while (av[i][n])
-			n++;
-		len += n;
-		i++;
-	}
-	return (len);
-}
-
-/**
- * ft_strlen - Calculates the length of a string.
- * @s: The input string.
- *
- * Return: The length of the string.
- */
-size_t ft_strlen(char *s)
-{
-	size_t i = 0;
-
-	while (s && s[i])
-		i++;
-	return (i);
-}
-
-/**
- * _strcpy - Copies the string pointed to by src to the buffer.
- * @dest: Pointer to the destination buffer.
- * @src: Pointer to the source string.
- *
- * Return: The pointer to dest.
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src && src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
- * argstostr - Concatenates all command line arguments into a single string.
- * @ac: The number of command line arguments.
- * @av: The array of command line arguments.
- *
- * Return: The concatenated string of all arguments, or NULL on failure.
+ * Return: A pointer to the concatenated string, or NULL if it fails.
  */
 char *argstostr(int ac, char **av)
 {
-	size_t i, b;
-	int in;
-	char *arr;
+	int i, j, len, index = 0;
+	char *str;
 
-	if (!ac || !av)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	arr = malloc(ft_p_strlen(av) + ac + 1);
-	if (!arr)
-		return (NULL);
-	in = 0;
-	arr[0] = 0;
-	while (++in < ac)
+	for (i = 0; i < ac; i++)
 	{
-		i = 0;
-		b = ft_strlen(arr);
-		while (i < ft_strlen(av[in]))
-			arr[b++] = av[in][i++];
-		arr[b++] = '\n';
+		j = 0;
+		while (av[i][j])
+		{
+			len++;
+			j++;
+		}
+		len++; /* For newline character */
 	}
-	arr[b] = 0;
-	return (arr);
+
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			str[index] = av[i][j];
+			index++;
+			j++;
+		}
+		str[index] = '\n';
+		index++;
+	}
+
+	str[index] = '\0';
+
+	return (str);
 }
