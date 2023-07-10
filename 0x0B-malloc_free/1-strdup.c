48332c8fafa350e5ed6808e9b214e9578c6987f8
root <root@inwi.168.1.22>
2023-07-10 12:52:20 -0400

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * ft_strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+size_t	ft_strlen(const	char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
  * ft_strdup - Duplicates a string.
  * @str: The string to be duplicated.
  *
