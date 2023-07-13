7a7ef6c12c1f85013047674bf2bdbb765d8e9e1f
root <root@inwi.168.1.22>
2023-07-13 18:26:22 -0400

    3

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,6 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void	ft_bzero(void *s, size_t n);
+int *array_range(int min, int max);
 
 #endif /*MAIN_H*/
