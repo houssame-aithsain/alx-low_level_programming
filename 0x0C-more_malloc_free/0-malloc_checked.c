433e62d6577466649b3a18df229c51006be2dd95
root <root@inwi.168.1.22>
2023-07-13 14:15:23 -0400

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory and checks if allocation was successful
+ * @b: Size of the memory to allocate
+ *
+ * This function uses the malloc function to allocate memory of size 'b'.
+ * If the allocation is successful,
+ * a pointer to the allocated memory is returned.
+ * If the allocation fails (malloc returns a null pointer),
+ * the function exits with status code 98.
+ *
+ * Return: Pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = (void *)malloc(b);
+	if (!p)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <limits.h>
+
+void *malloc_checked(unsigned int b);
+
+#endif /*MAIN_H*/
