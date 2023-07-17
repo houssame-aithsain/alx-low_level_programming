d741d5bbfcb0c46a9272a7097d23f9fbbf73174c
root <root@inwi.168.1.22>
2023-07-17 17:12:35 -0400

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,16 +16,16 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("nil\n");
+			printf("(nil)\n");
 
 		if (d->age)
 			printf("Age: %.1f\n", d->age);
 		else
-			printf("nil\n");
+			printf("(nil)\n");
 
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("nil\n");
+			printf("(nil)\n");
 	}
 }
