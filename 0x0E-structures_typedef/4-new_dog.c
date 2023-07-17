97dd94be7df91dbe7152e7113e44e7acfcc6155a
root <root@inwi.168.1.22>
2023-07-17 18:20:21 -0400

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,6 +60,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
+	if (!name || !owner)
+		return (NULL);
 	d->name = malloc(ft_strlen(name) + 1);
 	if (!d->name)
 	{
