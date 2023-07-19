d54e11fd0d5dcb3f3fd5c515d95a63852a2372bf
root <root@inwi.168.1.22>
2023-07-19 17:27:10 -0400

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,26 +17,20 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-
 	func = get_op_func(argv[2]);
-
-	if (func == NULL)
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	if ((*argv[2] == '/' || *argv[2] == '%') && !num2)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
 	result = func(num1, num2);
 	printf("%d\n", result);
-
 	return (0);
 }
