7bfd88e97e04f0673709b19e962f2e3cd98e56be
root <root@inwi.168.1.22>
2023-07-19 14:04:23 -0400

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,6 @@
 #include "function_pointers.h"
 
 /**
- * f - Prints a string character by character.
- * @name: The string to be printed.
- *
- * Return: None.
- */
-void f(char *name)
-{
-	for (int i = 0; name[i]; i++)
-		_putchar(name[i]);
-}
-
-/**
  * print_name - Prints a name using a given function pointer.
  * @name: The name to be printed.
  * @f: A function pointer that takes a character pointer and returns nothing.
