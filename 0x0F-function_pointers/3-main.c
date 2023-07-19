38729bd43a50852061b8c316cb598d99cb967174
root <root@inwi.168.1.22>
2023-07-19 17:02:50 -0400

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,66 +1,42 @@
 #include "3-calc.h"
 
 /**
- * _atoi - Converts a string to an integer
- * @s: The string to convert
+ * main - Entry point for the calculator program.
+ * @argc: The number of command-line arguments.
+ * @argv: Array of command-line argument strings.
  *
- * Return: The converted integer
+ * Return: Always 0.
  */
-int _atoi(char *s)
+int main(int argc, char *argv[])
 {
-	int sign = 1;
-	unsigned int result = 0;
-	int started = 0;
-
-	while (*s)
-	{
-		if (*s == '-')
-			sign *= -1;
-
-		if (*s >= '0' && *s <= '9')
-		{
-			result = result * 10 + (*s - '0');
-			started = 1;
-		}
-		else if (started)
-		{
-			break;
-		}
-
-		s++;
-	}
-
-	return (result * sign);
-}
-
-/**
- * main - Entry point of the program
- * @ac: The number of command-line arguments
- * @av: An array of strings containing the arguments
- *
- * Return: Always 0 (Success)
- */
-int main(int ac, char *av[])
-{
+	int num1, num2, result;
 	int (*func)(int, int);
 
-	if (ac != 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
-	if (av[2] && av[2][0] != '+' && av[2][0] != '-' && av[2][0] != '*'
-		&& av[2][0] != '/' && av[2][0] != '%')
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (func == NULL)
 	{
 		printf("Error\n");
-		exit(99);
+		return (99);
 	}
-	if ((av[2][0] == '/' || av[2][0] == '%') && av[3][0] == 48)
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
-		exit(100);
+		return (100);
 	}
-	func = get_op_func(av[2]);
-	printf("%d\n", func(_atoi(av[1]), _atoi(av[3])));
+
+	result = func(num1, num2);
+	printf("%d\n", result);
+
 	return (0);
 }
