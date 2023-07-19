d421e06896078aa3ef8cf58059b62bec89395ae7
root <root@inwi.168.1.22>
2023-07-19 14:12:53 -0400

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Applies a given function to each element of an array.
+ * @array: Pointer to the array.
+ * @size: Size of the array.
+ * @action: Pointer to a function that takes an integer argument
+ * and returns void.
+ *
+ * Return: None.
+ *
+ * Description: This function applies the function pointed to by 'action'
+ * to each element of the 'array'.
+ * If any of the input arguments is NULL or size is 0,
+ * the function returns without performing any action.
+ * Otherwise, it iterates through the array and applies the 'action'
+ * function to each element of the array.
+ * The 'action' function will determine what operation is performed
+ * on each element of the array.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (!array || !action || !size)
+		return;
+
+	for (size_t i = 0; i < size; i++)
+		action(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,6 +6,6 @@
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void f(char *name);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /*FUNCTION_POINTERS_H*/
