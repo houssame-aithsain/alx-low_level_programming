abfc5cc406717d6652006c27b31cee3f4b0f69de
root <root@inwi.168.1.22>
2023-07-20 19:14:24 -0400

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,9 +19,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
-		if (i < n - 1 && separator)
+		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
-	}
 	va_end(args);
 	printf("\n");
 }
