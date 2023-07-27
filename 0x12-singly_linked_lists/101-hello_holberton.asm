fe4da9d2f13ae68922689df4b90ca7fbb1314b78
Houssame Ait Hsain <houssame1337@gmail.com>
2023-07-27 16:36:10 -0400

    101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .data
+    format db "Hello, Holberton", 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    mov edi, format     ; First argument: format string
+    xor eax, eax        ; Clear EAX (EAX=0 is the syscall number for sys_exit)
+    call printf         ; Call the C printf function
+
+    mov eax, 0          ; Return 0 (exit status 0)
+    ret                 ; Return from the main function
