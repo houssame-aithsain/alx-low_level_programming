bc0d199b24d1d63a903b9992d16e33bb9c778d0a
Houssame Ait Hsain <houssame1337@gmail.com>
2023-07-27 13:22:30 -0400

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: Pointer to the head of the linked list.
+ */
+void free_list(list_t *head)
+{
+    list_t  *cnt;
+    list_t  *tmp;
+
+    cnt = head;
+    while (cnt)
+    {
+        tmp = cnt->next;
+        free(cnt->str);
+        free(cnt);
+        cnt = tmp;
+    }
+}
