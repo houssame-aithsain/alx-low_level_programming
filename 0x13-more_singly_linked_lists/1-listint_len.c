ec03fa9211e40c0a40659a4c3b532de0dacd3ac5
Houssame Ait Hsain <houssame1337@gmail.com>
2023-07-31 11:32:27 -0400

    1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+ /**
+  * listint_len - Returns the number of elements in a linked listint_t list.
+ * @h: The head of the linked list.
+ *
+ * This function takes a pointer to the head of a linked list and
+ * returns the number of nodes in the list. It uses a while loop
+ * to traverse the list, incrementing the 'len' variable for each node
+ * until it reaches the end of the list (h becomes NULL). The final
+ * value of 'len' represents the total number of nodes in the list,
+ * which is then returned as the result.
+ *
+ * Return: The number of nodes in the linked list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len;
+
+	len = 0;
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
