cde5740d69380ea4f1ef4d056a4359c2a4db1645
Houssame Ait Hsain <houssame1337@gmail.com>
2023-07-31 12:40:14 -0400

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -17,9 +17,9 @@ void free_listint2(listint_t **head)
 	while (head && *head)
 	{
 		tmp = *head;
-		head = (*head)->next;
+		*head = (*head)->next;
 		free(tmp);
 	}
-	free(head);
 	head = NULL;
 }
+
