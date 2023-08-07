5a97bb6742e7290ccae7ecd5de0892475af2faa3
Houssame Ait Hsain <houssame1337@gmail.com>
2023-08-07 16:26:01 -0400

    4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,8 +37,8 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_from == -1)
 		print_error("Error: Can't read from file", argv[1], 98, -1);
 	if (fd_to == -1)
@@ -49,7 +49,7 @@ int main(int argc, char **argv)
 		if (rd == -1)
 			print_error("Error: Can't read from file", argv[1], 98, -1);
 		buffer[rd] = 0;
-		if (write(fd_to, buffer, BUFFER_SIZE) == -1)
+		if (write(fd_to, buffer, rd) == -1)
 			print_error("Error: Can't write to", argv[2], 99, -1);
 	}
 	if (close(fd_from) == -1)
