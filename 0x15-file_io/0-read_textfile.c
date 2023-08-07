304c66953714e72b33da086af604bbcd7c75b259
Houssame Ait Hsain <houssame1337@gmail.com>
2023-08-07 12:44:53 -0400

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
- * ft_print_buffer - Prints the contents of a buffer to the standard output.
- * @buffer: The buffer to print.
- * Return: The number of characters written.
- */
-int ft_print_buffer(char *buffer)
-{
-	int i;
-
-	i = 0;
-	while (buffer && buffer[i])
-	{
-		if (write(1, &buffer[i++], 1) < 0)
-		{
-			free(buffer);
-			return (0);
-		}
-	}
-	free(buffer);
-	return (i);
-}
-
-/**
  * read_textfile - Reads and prints a specified number of letters from a file.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
@@ -31,27 +9,26 @@ int ft_print_buffer(char *buffer)
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	ssize_t fd, rd;
+	ssize_t fd, rd, wr;
 
-	if (!filename || !letters)
+	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-	buffer = malloc(letters + 1);
+	buffer = malloc(letters);
 	if (!buffer)
-	{
-		close(fd);
 		return (0);
-	}
 	rd = read(fd, buffer, letters);
 	if (rd < 0)
 	{
 		free(buffer);
-		close(fd);
 		return (0);
 	}
 	close(fd);
 	buffer[rd] = 0;
-	return (ft_print_buffer(buffer));
+	wr = write(STDOUT_FILENO, &buffer, rd);
+	if (wr < 0)
+		return (0);
+	return (wr);
 }
