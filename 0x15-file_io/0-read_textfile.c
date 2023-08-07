f7176fc51f7c1f50217904f779dd6feef9fa3994
Houssame Ait Hsain <houssame1337@gmail.com>
2023-08-07 12:22:52 -0400

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,7 +27,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t fd, rd;
 
-	if (!filename)
+	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
