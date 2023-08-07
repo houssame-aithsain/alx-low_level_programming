6d3134f61ab95b45ae07cc01d07a83001c5a63f8
Houssame Ait Hsain <houssame1337@gmail.com>
2023-08-07 13:01:19 -0400

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file with specific permissions
+ * and writes content to it.
+ * @filename: The name of the file to create.
+ * @text_content: The content to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bw = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		while (text_content[bw])
+			bw++;
+		bw = write(fd, text_content, bw);
+		if (bw == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
