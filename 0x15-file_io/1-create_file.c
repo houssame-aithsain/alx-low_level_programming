0b3f9bfeef6a7552d646edb937da9b430380bef2
Houssame Ait Hsain <houssame1337@gmail.com>
2023-08-07 13:08:06 -0400

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,19 +10,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wt, bw = 0;
+	int fd, bw = 0;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
+	if (text_content)
 	{
 		while (text_content[bw])
 			bw++;
-		wt = write(fd, &text_content, bw);
-		if (wt != bw)
+		write(fd, &text_content, bw);
+		if (bw == -1)
 		{
 			close(fd);
 			return (-1);
