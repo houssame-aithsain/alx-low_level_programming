b5a866fc229e9e619e71d4e7b03dce3803df4299
Houssame Ait Hsain <houssame1337@gmail.com>
2023-08-07 12:54:27 -0400

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,44 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads and prints a specified number of letters from a file.
+ * read_textfile - Reads and prints the contents of a text file.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
- * Return: The number of characters written, or 0 on failure.
+ *
+ * Return: The actual number of letters read and printed.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t bd, bw;
 	char *buffer;
-	ssize_t fd;
-	int rd, wr;
 
-	if (!filename || !letters)
+	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
-	buffer = malloc(letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
-		return (0);
-	rd = read(fd, buffer, letters);
-	if (rd < 0)
 	{
-		free(buffer);
+		close(fd);
 		return (0);
 	}
-	buffer[rd] = 0;
-	close(fd);
-	wr = write(STDOUT_FILENO, &buffer, rd);
-	if (wr < 0)
+	bd = read(fd, buffer, letters);
+	if (bd == -1)
 	{
 		free(buffer);
+		close(fd);
+		return (0);
+	}
+	bw = write(STDOUT_FILENO, buffer, bd);
+	if (bw == -1 || bw != bd)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
 	}
 	free(buffer);
-	return (wr);
+	close(fd);
+	return (bw);
 }
