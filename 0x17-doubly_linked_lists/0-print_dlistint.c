69d2920703e449e422af789767686b3202162364
Houssame Ait Hsain <houssame1337@gmail.com>
2023-09-15 14:26:21 +0100

    100%

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,13 +11,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
