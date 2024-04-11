123f34338baaabf47294092dfefe82d0d3fb3e8e
Houssame Ait hsain <hait-hsa@c1r2p9.1337.ma>
2024-04-11 12:00:41 +0000

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,1 +1,12 @@
-#include
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *hs = malloc(sizeof(hash_node_t) * size);
+    return (hs) ? hs : NULL;
+}
