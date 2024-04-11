e5f184c6f7555a816c61a305ff75ec3ec4c6d7ef
Houssame Ait hsain <hait-hsa@c1r2p9.1337.ma>
2024-04-11 12:03:10 +0000

    x

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,6 +7,6 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hs = malloc(sizeof(hash_node_t) * size);
-    return (hs) ? hs : NULL;
+	hash_table_t *hs = malloc(sizeof(hash_node_t) * size);
+	return (hs) ? hs : NULL;
 }
