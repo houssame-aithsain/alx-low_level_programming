1e0d52527c19e0b0623991abf16e5c138cac329d
Houssame <hossame@inbox.ru>
2024-06-02 12:23:25 +0100

    5-island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in the grid.
@@ -15,16 +16,12 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                # Check top neighbor
                 if i == 0 or grid[i-1][j] == 0:
                     perimeter += 1
-                # Check bottom neighbor
                 if i == rows-1 or grid[i+1][j] == 0:
                     perimeter += 1
-                # Check left neighbor
                 if j == 0 or grid[i][j-1] == 0:
                     perimeter += 1
-                # Check right neighbor
                 if j == cols-1 or grid[i][j+1] == 0:
                     perimeter += 1
 
