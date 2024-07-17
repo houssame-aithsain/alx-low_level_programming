01fd2f2132008ff60e1cfb9eb84209176299e12c
Elpira <elpira@elpira.168.1.36>
2024-07-17 12:17:47 -0400

    8

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,8 @@
 int linear_search(int *array, size_t size, int value);
 void print_array(const int *array, size_t size);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+
 
 #endif /*SEARCH_ALGOS_H*/
